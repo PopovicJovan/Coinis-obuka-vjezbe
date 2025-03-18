30545871c57f6ea5287ec38a4d68198dc4590fed
PopovicJovan <jovan6.p@gmail.com>
2025-03-18 22:27:02 +0100

    search dodat za primjer2 listu

--- noc2-vjezba/primjer2.js
+++ noc2-vjezba/primjer2.js
@@ -18,12 +18,30 @@ let addButton = document.getElementById("add");
 addButton.addEventListener("click", () => {
     let inputValue = addButton.parentElement.querySelector("input").value;
     document.querySelector("#items").innerHTML += `
-    <div class="w-100 p-3 d-flex justify-content-between align-items-center border">
+    <div class="w-100 p-3 d-flex justify-content-between align-items-center border ">
                 <h5>${inputValue}</h5>
                 <button type="button" class="btn-close bg-danger py-2 px-3 exit" aria-label="Close">X</button>
-    </div>
-    `;
+    </div>`;
     exitButtons = document.querySelectorAll(".exit");
     addEventOnButton(exitButtons);
-
 })
+
+const findMatching = (e) => {
+    let inputValue = e.target.value;
+    let itemsDiv = document.getElementById("items").querySelectorAll("h5");
+    for (let i of itemsDiv){
+        i.parentElement.classList.remove("border-danger");
+    }
+    if (!inputValue) return;
+    for (let i of itemsDiv){
+        if (i.innerText.includes(inputValue)){
+            console.log(i);
+            i.parentElement.classList.add("border-danger");
+        }
+    }
+
+}
+
+
+let searchInput = document.getElementById("search");
+searchInput.addEventListener("input", (e) => findMatching(e))
